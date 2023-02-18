7381b2fcce051e55c22d1280c820605151595527
Jeffery Bennett <meandmybadself@gmail.com>
2023-02-17 22:40:50 -0600

    Adds styling

--- src/bingo-board.ts
+++ src/bingo-board.ts
@@ -51,17 +51,39 @@ export class BingoBoard extends LitElement {
     this._startGame()
   }
 
-  private _startGame() {
-    this._markedNumbers = []
-    this._set = generateSet()
-    this._currentLetterNumber = this._getRandomLetterNumber()
+  private _startGame(restart = false) {
+    this._makeFullscreen()
+    if (!restart && localStorage.getItem('set')) {
+      this._set = JSON.parse(localStorage.getItem('set')!)
+      this._markedNumbers = JSON.parse(localStorage.getItem('markedNumbers')!)
+      this._currentLetterNumber = JSON.parse(localStorage.getItem('currentLetterNumber')!)
+    } else {
+      this._set = generateSet()
+      this._markedNumbers = []
+      this._currentLetterNumber = undefined
+    }
+    // this._currentLetterNumber = this._getRandomLetterNumber()
+    this._persistState()
   }
 
-  private _getRandomLetterNumber(): LetterNumber {
 
+  private _persistState() {
+    if (this._set) {
+      localStorage.setItem('set', JSON.stringify(this._set))
+    }
+    if (this._markedNumbers) {
+      localStorage.setItem('markedNumbers', JSON.stringify(this._markedNumbers))
+    }
+    if (this._currentLetterNumber) {
+      localStorage.setItem('currentLetterNumber', JSON.stringify(this._currentLetterNumber))
+    }
+  }
+
+
+  private _getRandomLetterNumber(): LetterNumber {
     // If the set is empty, start a new game.
     if (!this._set.length) {
-      this._startGame()
+      this._startGame(true)
     }
 
     const randomLetterIndex = Math.floor(Math.random() * this._set.length)
@@ -85,20 +107,39 @@ export class BingoBoard extends LitElement {
     }
   }
 
+  _makeFullscreen() {
+    // Make full screen.
+    const el = this.shadowRoot?.querySelector('.board')
+    if (el) {
+      el.requestFullscreen()
+    }
+  }
+
+  _showHelp() {
+
+  }
+
   render() {
     return html`
       <div class="board">
-        <ul class="letters" @click=${() => this._startGame()}>
+        <ul role="button" class="letters" @click=${() => this._startGame(true)}>
           ${letters.map(letter => html`<li>${letter}</li>`)}
         </ul>
         <ul class="numbers">
           ${letters.map(letter => html`<li><ul>${getRangeForLetter(letter).map(number => html`<li class="number ${this._markedNumbers.includes(number) ? 'marked' : ''}">${number}</li>`)}</ul></li>`)}
         </ul>
         <div class="current"
-          @click=${() => this._currentLetterNumber = this._getRandomLetterNumber()}
+          role="button"
+          @click=${() => {
+
+        this._makeFullscreen()
+        this._currentLetterNumber = this._getRandomLetterNumber()
+        this._persistState()
+      }}
         >
           <div class="letter">${this._currentLetterNumber?.letter}</div>
           <div class="number">${this._currentLetterNumber?.number}</div>
+          <div class="help" role="button" @click=${() => this._showHelp()}>?</div>
         </div>
       </div>
     `
